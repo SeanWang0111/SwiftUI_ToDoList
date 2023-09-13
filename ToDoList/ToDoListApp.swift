7549b8823fcde0642fd877905586f364a20cf72c
SeanWang <seanwang0111@gmail.com>
2023-09-13 17:24:44 +0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by 王奕翔 on 2023/9/13.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
