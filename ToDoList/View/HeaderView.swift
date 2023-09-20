f4eabdc70306f6953a11c2c1d10915b8ed4e3b10
SeanWang <seanwang0111@gmail.com>
2023-09-20 23:43:48 +0800

    Lesson 3: Header View

--- ToDoList/View/HeaderView.swift
+++ ToDoList/View/HeaderView.swift
@@ -11,30 +11,38 @@ struct HeaderView: View {
     
     private let width: CGFloat = UIScreen.main.bounds.width
     
+    let title: String
+    let subtitle: String
+    let angle: Double
+    let background: Color
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 0)
-                .foregroundColor(.pink)
-                .rotationEffect(Angle(degrees: 15))
+                .foregroundColor(background)
+                .rotationEffect(Angle(degrees: angle))
             
             VStack {
-                Text("To Do List")
+                Text(title)
                     .font(.system(size: 50))
                     .bold()
                 
-                Text("Get things done")
+                Text(subtitle)
                     .font(.system(size: 30))
             }
             .foregroundColor(.white)
-            .padding(.top, 30)
+            .padding(.top, 80)
         }
-        .frame(width: width * 3, height: 300)
-        .offset(y: -100)
+        .frame(width: width * 3, height: 350)
+        .offset(y: -150)
     }
 }
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
+        HeaderView(title: "title",
+                   subtitle: "subtitle",
+                   angle: 15,
+                   background: .blue)
     }
 }
